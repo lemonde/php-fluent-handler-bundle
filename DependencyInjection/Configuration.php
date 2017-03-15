74ca98a3bdde78c9bb7ae5c99fa3f3d6619b6c3a
Jonathan Jalouzot <jalouzot@lemonde.fr>
2017-03-15 09:39:11 +0100

    feat(log): add channel in option of log

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,6 +22,7 @@ class Configuration implements ConfigurationInterface
 
         $rootNode
             ->children()
+                ->scalarNode('channel')->end()
                 ->scalarNode('host')->end()
                 ->integerNode('port')->end()
             ->end()
